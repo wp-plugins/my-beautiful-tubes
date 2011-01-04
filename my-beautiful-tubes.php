da0179573a2b338b8f9685ed760b257dabe073ff
GadgetsChoose <GadgetsChoose@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-04 05:18:56 +0000

    Committing version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/my-beautiful-tubes/trunk@328501 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- my-beautiful-tubes.php
+++ my-beautiful-tubes.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: my beautiful tubes
-Plugin URI: http://todayprofits.gadgets-code.com/2010/12/24/my-beautiful-tubes/
+Plugin URI: http://todayprofits.gadgets-code.com/2011/01/04/my-beautiful-tubes-1-3
 Description: A plugin which allows blogger to embed youtube video on the post
-Version: 1.2
+Version: 1.3
 Author: Gadgets-Code.Com
 Author URI: http://todayprofits.gadgets-code.com
 */
@@ -43,6 +43,9 @@ function tube_meta_box($post,$box) {
   $tube_wt = get_post_meta($post->ID,'_tubes_wt',true);
   $tube_ht = get_post_meta($post->ID,'_tubes_ht',true);
   $tube_position = get_post_meta($post->ID,'_tubes_position',true);
+  $tube_rel_title = get_post_meta($post->ID,'_tubes_rel_title',true);
+  $tube_rel_des = get_post_meta($post->ID,'_tubes_rel_des',true);
+  $tube_rel_link = get_post_meta($post->ID,'_tubes_rel_link',true);
 
   echo '<p>'.__('Video URL','tube-plugin'). ': <input type="text" name="tubes_url" value="'.esc_attr($tube_url).'"/></p>
         <p>'.__('Video Width','tube-plugin'). ': <input type="text" name="tubes_wt" value="'.esc_attr($tube_wt).'"/></p>
@@ -51,7 +54,10 @@ function tube_meta_box($post,$box) {
         <option value="top-left" '.(is_null($tube_position) || $tube_position=='top-left' ? 'selected="selected"' : '').'>Top Left</option>
         <option value="top-right" '.($tube_position=='top-right' ? 'selected="selected"' : '').'>Top Right</option>
         <option value="bottom-left" '.($tube_position=='bottom-left' ? 'selected="selected"' : '').'>Bottom Left</option>
-       <option value="bottom-right" '.($tube_position=='bottom-right' ? 'selected="selected"' : '').'>Bottom Right</option></select></p>';
+        <option value="bottom-right" '.($tube_position=='bottom-right' ? 'selected="selected"' : '').'>Bottom Right</option></select></p>
+        <p>'.__('Links Title','tube-plugin'). ': <input type="text" name="tubes_rel_title" value="'.esc_attr($tube_rel_title).'"/></p>
+        <p>'.__('Related Links Description','tube-plugin'). ':<br/><textarea name="tubes_rel_des" rows="15" cols="30">'.esc_attr($tube_rel_des).'</textarea></p>
+        <p>'.__('Related Links','tube-plugin'). ':<br/><textarea name="tubes_rel_link" rows="15" cols="30">'.esc_attr($tube_rel_link).'</textarea></p>';
 
 }
 
@@ -66,6 +72,9 @@ function tube_save_meta_box($post_id) {
     update_post_meta($post_id,'_tubes_wt',esc_attr($tubeW));
     update_post_meta($post_id,'_tubes_ht',esc_attr($tubeH));
     update_post_meta($post_id,'_tubes_position',esc_attr($_POST['tubes_position']));
+    update_post_meta($post_id,'_tubes_rel_title',esc_attr($_POST['tubes_rel_title']));
+    update_post_meta($post_id,'_tubes_rel_des',esc_attr($_POST['tubes_rel_des']));
+    update_post_meta($post_id,'_tubes_rel_link',esc_attr($_POST['tubes_rel_link']));
 
   }
 
@@ -73,11 +82,17 @@ function tube_save_meta_box($post_id) {
 
 function displays_video($content) {
 
+
    $art_id = get_the_ID();
    $video_url = get_post_meta($art_id,'_tubes_url',true);
    $video_position = get_post_meta($art_id,'_tubes_position',true);
    $vd_W = get_post_meta($art_id,'_tubes_wt',true);
    $vd_H = get_post_meta($art_id,'_tubes_ht',true);
+   $tube_sel_title = get_post_meta($art_id,'_tubes_rel_title',true);
+   $tube_sel_description = get_post_meta($art_id,'_tubes_rel_des',true);
+   $tube_sel_link =  get_post_meta($art_id,'_tubes_rel_link',true);
+   $sel_description = explode(",",$tube_sel_description);
+   $sel_link = explode(",",$tube_sel_link);
 
    if (is_single() || is_home()) {
 
@@ -90,42 +105,63 @@ function displays_video($content) {
          $vidtube = $vidtu;
          $vidtube = str_replace("=","/",str_replace("watch?","",$vidtube));
          $tweet_vid = "Watch this video at $vidtu or visit the article site on ";
-
+         $addup = "";
           if($video_position=='top-left') {
 
            $vidtubes = "<div style=\"float:left;margin-right:6px;margin-top:7px;margin-bottom:2px;\">"."<object width=$vd_W height=$vd_H><param name=\"movie\" value=$vidtube></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=$vidtube type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=$vd_W height=$vd_H></embed></object>".
-                       "<br/><script type=\"text/javascript\" src=\"http://platform.twitter.com/widgets.js\"></script>
-                       <a href=\"http://twitter.com/share\"  class=\"twitter-share-button\"
-                       data-text=\"$tweet_vid\"
-                       data-count=\"none\">Tweet</a></div>";
-           return $vidtubes.$content;
+                       "<br/><form name=\"selection\"><select name=\"relatedLink\" onchange=\"browseTo()\" size=\"1\" style=\"background:grey;color:white;font-size:17px;\"><option selected=\"selected\" value=\"\">$tube_sel_title</option>";
+
+                       for($selOpt=0;$selOpt<sizeof($sel_description);$selOpt++) {
+                       $decsrp = $sel_description[$selOpt];
+                       $declink = $sel_link[$selOpt];
+                       $addup.="<option value=$declink>$decsrp</option>";
+                       }
+                       $addup.="</select></form></div>";
+
+            return $vidtubes.$addup.$content;
 
            } elseif ($video_position=='top-right') {
 
            $vidtubes = "<div style=\"float:right;margin-left:6px;margin-top:7px;margin-bottom:2px;\">"."<object width=$vd_W height=$vd_H><param name=\"movie\" value=$vidtube></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=$vidtube type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=$vd_W height=$vd_H></embed></object>".
-                       "<br/><script type=\"text/javascript\" src=\"http://platform.twitter.com/widgets.js\"></script>
-                        <a href=\"http://twitter.com/share\"  class=\"twitter-share-button\"
-                        data-text=\"$tweet_vid\"
-                        data-count=\"none\">Tweet</a></div>";
-           return $vidtubes.$content;
+                       "<br/><form name=\"selection\"><select name=\"relatedLink\" onchange=\"browseTo()\" size=\"1\" style=\"background:grey;color:white;font-size:17px;\"><option selected=\"selected\" value=\"\">$tube_sel_title</option>";
+
+                       for($selOpt=0;$selOpt<sizeof($sel_description);$selOpt++) {
+                       $decsrp = $sel_description[$selOpt];
+                       $declink = $sel_link[$selOpt];
+                       $addup.="<option value=$declink>$decsrp</option>";
+                       }
+                       $addup.="</select></form></div>";
+
+            return $vidtubes.$addup.$content;
 
            } elseif ($video_position=='bottom-left') {
 
            $vidtubes = "<div style=\"float:left;margin-right:6px;margin-top:3px;margin-bottom:2px;\">"."<object width=$vd_W height=$vd_H><param name=\"movie\" value=$vidtube></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=$vidtube type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=$vd_W height=$vd_H></embed></object>".
-                       "<br/><script type=\"text/javascript\" src=\"http://platform.twitter.com/widgets.js\"></script>
-                        <a href=\"http://twitter.com/share\"  class=\"twitter-share-button\"
-                        data-text=\"$tweet_vid\"
-                        data-count=\"none\">Tweet</a></div>";
-           return $content.$vidtubes;
+                       "<br/><form name=\"selection\"><select name=\"relatedLink\" onchange=\"browseTo()\" size=\"1\" style=\"background:grey;color:white;font-size:17px;\"><option selected=\"selected\" value=\"\">$tube_sel_title</option>";
+
+                       for($selOpt=0;$selOpt<sizeof($sel_description);$selOpt++) {
+                       $decsrp = $sel_description[$selOpt];
+                       $declink = $sel_link[$selOpt];
+                       $addup.="<option value=$declink>$decsrp</option>";
+                       }
+                       $addup.="</select></form></div>";
+
+             return $content.$vidtubes.$addup;
 
            } elseif ($video_position=='bottom-right') {
 
            $vidtubes = "<div style=\"float:right;margin-left:6px;margin-top:3px;margin-bottom:2px;\">"."<object width=$vd_W height=$vd_H><param name=\"movie\" value=$vidtube></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=$vidtube type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=$vd_W height=$vd_H></embed></object>".
-                       "<br/><script type=\"text/javascript\" src=\"http://platform.twitter.com/widgets.js\"></script>
-                       <a href=\"http://twitter.com/share\"  class=\"twitter-share-button\"
-                       data-text=\"$tweet_vid\"
-                       data-count=\"none\">Tweet</a></div>";
-           return $content.$vidtubes;
+                       "<form name=\"selection\"><select name=\"relatedLink\" onchange=\"browseTo()\" size=\"1\" style=\"background:grey;color:white;font-size:17px;\"><option selected=\"selected\" value=\"\">$tube_sel_title</option>";
+
+                       for($selOpt=0;$selOpt<sizeof($sel_description);$selOpt++) {
+                       $decsrp = $sel_description[$selOpt];
+                       $declink = $sel_link[$selOpt];
+                       $addup.="<option value=$declink>$decsrp</option>";
+                       }
+                       $addup.="</select></form></div>";
+
+
+             return $content.$vidtubes.$addup;
 
            } } else { return $content; }
 
@@ -134,6 +170,28 @@ function displays_video($content) {
 }
 
   add_filter('the_content','displays_video');
+
+  function gotosite() {
+
+  echo '<script type="text/javascript">
+
+   function browseTo() {
+
+   var newUrl = document.selection.relatedLink;
+
+   if(newUrl.options[newUrl.selectedIndex].value) {
+    location.href=newUrl.options[newUrl.selectedIndex].value;
+   } else {return false;}
+
+   }
+
+   </script>';
+
+   }
+
+
+  add_action('wp_footer','gotosite');
+
 
   function my_beautiful_tubes_deactivate(){
      $theseblog = get_bloginfo('url');
